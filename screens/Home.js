c67c06f79f44382b39de84fa9837b53cc996048f
Abe <abhi.rawat456@gmail.com>
2020-09-19 18:47:22 +0530

    Changed Home Screen Design

--- screens/Home.js
+++ screens/Home.js
@@ -7,6 +7,7 @@ import {
   Image,
   YellowBox,
   ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 import { UserContext } from "../context/UserContext";
 
@@ -54,30 +55,32 @@ function Home() {
           your favorite xkcd comics.
         </Text>
       </View>
-      <Text style={styles.topic}>Latest Comic</Text>
-      <View style={styles.comicContainer}>
-        {loading ? (
-          <ActivityIndicator />
-        ) : (
-          <>
-            <View style={styles.imageContainer}>
-              <Image
-                resizeMode="cover"
-                source={{ uri: imageUrl }}
-                style={styles.image}
-              />
+      <View style={styles.mainContainer}>
+        <View style={styles.bottomContainer}>
+          <Text style={styles.topic}>Latest Comic</Text>
+          <View style={styles.imageContainer}>
+            <Image
+              resizeMode="cover"
+              source={{ uri: imageUrl }}
+              style={styles.image}
+            />
+          </View>
+          <View style={styles.comicInfo}>
+            <View style={styles.pill}>
+              <Text style={styles.pillText}>#{comicNumber}</Text>
             </View>
-            <View style={styles.comicInfo}>
-              <View style={styles.pill}>
-                <Text style={styles.pillText}>#{comicNumber}</Text>
-              </View>
-              <Text style={styles.pillTitle}>{comicTitle}</Text>
-              <Text style={styles.pillSubTitle}>
-                {`Date created : ${comicDate}`}
-              </Text>
-            </View>
-          </>
-        )}
+            <Text style={styles.pillTitle}>{comicTitle}</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.bottomContainer}>
+          <View style={styles.exploreView}>
+            <Image
+              source={require("../assets/right-arrow-button.png")}
+              style={styles.exploreImage}
+            />
+            <Text style={styles.exploreText}>Explore</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -117,36 +120,61 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   topic: {
-    paddingLeft: 30,
-    marginTop: 30,
-    fontWeight: "bold",
     fontSize: 20,
-  },
-  comicContainer: {
+    fontWeight: "bold",
+    textAlign: "center",
     marginTop: 20,
-    marginHorizontal: 20,
-    borderWidth: 1,
-    borderRadius: 10,
-    borderColor: "#eee",
-    padding: 20,
+  },
+  mainContainer: {
     flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  bottomContainer: {
+    marginTop: 30,
+    flex: 1,
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    borderRadius: 15,
+    borderColor: "#eee",
+    height: 270,
+    elevation: 2,
+    marginHorizontal: 5,
   },
   imageContainer: {
+    marginVertical: 15,
     borderRadius: 5,
-    height: 85,
-    width: 85,
-    backgroundColor: "#eee",
-    justifyContent: "center",
-    alignItems: "center",
-    marginRight: 30,
+    height: 90,
+    alignSelf: "center",
   },
   image: {
     borderRadius: 5,
-    height: 80,
-    width: 80,
+    height: 90,
+    width: 150,
+  },
+  exploreView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  exploreImage: {
+    width: 40,
+    height: 40,
+  },
+  exploreText: {
+    marginTop: 40,
+    fontSize: 20,
   },
   comicInfo: {
     justifyContent: "space-between",
+    marginTop: 10,
+    paddingLeft: 10,
   },
   pill: {
     backgroundColor: "#D1E9FE",
@@ -161,13 +189,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   pillTitle: {
+    marginTop: 5,
     fontWeight: "bold",
     fontSize: 20,
-  },
-  pillSubTitle: {
-    fontWeight: "bold",
-    fontSize: 14,
-    opacity: 0.5,
   },
 });
 
