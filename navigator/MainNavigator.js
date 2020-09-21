0c2e31c1a31cd0f2ed7c6f70f56fb8cc67d951fe
Abe <abhi.rawat456@gmail.com>
2020-09-21 12:53:11 +0530

    Added loading screen

--- navigator/MainNavigator.js
+++ navigator/MainNavigator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { firebase } from "../firebase/firebase";
 import { getUserData } from "../firebase/functions";
 import Navigator from "./Navigator";
@@ -6,8 +6,10 @@ import AuthNavigator from "./AuthNavigator";
 import { AuthContext } from "../context/AuthContext";
 import { UserContext } from "../context/UserContext";
 import { FavoriteContext } from "../context/FavoriteContext";
+import { ActivityIndicator, View } from "react-native";
 
 function MainNavigator() {
+  const [loading, setLoading] = useState(true);
   const { loggedIn, logIn, logOut } = useContext(AuthContext);
   const { changeUser, emptyUser } = useContext(UserContext);
   const { initializeFavs, emptyFavs } = useContext(FavoriteContext);
@@ -24,10 +26,25 @@ function MainNavigator() {
         emptyUser();
         emptyFavs();
       }
+      setLoading(false);
     });
   }, []);
 
-  return <>{loggedIn ? <Navigator /> : <AuthNavigator />}</>;
+  return (
+    <>
+      {loading ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" />
+        </View>
+      ) : loggedIn ? (
+        <Navigator />
+      ) : (
+        <AuthNavigator />
+      )}
+    </>
+  );
 }
 
 export default MainNavigator;
